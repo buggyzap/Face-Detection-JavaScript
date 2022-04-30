63ff97ddbf7a15294af5b46f89d21af606a6fc83
Marco Ingraiti <marco.ingraiti@icloud.com>
2022-05-01 00:15:00 +0200

    added ext link

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('https://immedia-event.dgcal.it/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('https://immedia-event.dgcal.it/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('https://immedia-event.dgcal.it/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('https://immedia-event.dgcal.it/models')
 ]).then(startVideo)
 
 function startVideo() {
-  navigator.getUserMedia(
+  navigator.mediaDevices.getUserMedia(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
   )
 }
 
-video.addEventListener('play', () => {
+video.addEventListener('playing', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
